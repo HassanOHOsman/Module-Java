8e927ccc1a213a02d01a551d36af9285c6f5eb37
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-07-03 00:07:43 +0100

    Track if a digit is a prime number by first assuming that it's a prime number and if the condition proved i is divisble by j then the digit isn't a prime number and the code should break

--- PrimeNumbers.java
+++ PrimeNumbers.java
@@ -1,13 +1,21 @@
 public class PrimeNumbers {
     private static void getPrimeNumbers() {
         for (int i = 1; i <= 1000; i++) {
+            boolean isPrimeNumber = true;
             for (int j = 2; j < i; j++) {
+                if (i  % j == 0) {
+                    isPrimeNumber = false;
+                    break;
+                }
+                }
 
             }
         }
 
-    }
-    static void main(String[] args) {
+        static void main(String[] args) {
+
+        }
 
     }
-}
+
+
