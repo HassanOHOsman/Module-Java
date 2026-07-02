6c851a86b08bdd529869d83757f89df4ceea14a4
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-07-02 23:59:56 +0100

    Create the skeleton for the private method to print out all prime numbers

--- PrimeNumbers.java
+++ PrimeNumbers.java
@@ -1,5 +1,10 @@
 public class PrimeNumbers {
     private static void getPrimeNumbers() {
+        for (int i = 1; i <= 1000; i++) {
+            for (int j = 2; j < i; j++) {
+
+            }
+        }
 
     }
     static void main(String[] args) {
