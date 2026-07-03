84d3022aa66bf19e4abe3bc6b7186133d7e341cb
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-07-03 22:25:47 +0100

    Refactor code by extracting a helper function the is responsible for checker if a number is prime or not

--- PrimeNumbers.java
+++ PrimeNumbers.java
@@ -1,15 +1,20 @@
 public class PrimeNumbers {
+    //
+    private static boolean isPrime(int number) {
+        for (int j = 2; j < number; j++) {
+            if (number  % j == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     // the method prints out all prime numbers between 1 and 1000 (inclusive).
 
     private static void getPrimeNumbers() {
         for (int i = 1; i <= 1000; i++) {
-            boolean isPrimeNumber = true;
-            for (int j = 2; j < i; j++) {
-                if (i  % j == 0) {
-                    isPrimeNumber = false;
-                    break;
-                }
-            }
+
+
             if (isPrimeNumber) {
                 System.out.println(i);
             }
