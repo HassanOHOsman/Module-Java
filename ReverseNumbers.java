e87fee1a49bed57edcdd910a48e4d61f130b4d71
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-07-03 22:51:26 +0100

    Create javadoc describing the reverseNumber() method, its parameter and its return.

--- ReverseNumbers.java
+++ ReverseNumbers.java
@@ -1,6 +1,13 @@
 public class ReverseNumbers {
 
+    /**
+     * reverseNumber() method takes a number and return its reverse
+     * @param number is an integer
+     * @return is the reverse of that integer
+     */
+
     private static int reverseNumber(int number) {
+
 
     }
 
