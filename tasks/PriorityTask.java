9c887d10e968bec028a79aabedde04839ffe4623
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-07-11 15:42:35 +0100

    Create the 1st specialised class: TimedTask and populate it with its own unique properties.

---
+++ tasks/PriorityTask.java
@@ -0,0 +1,5 @@
+package tasks;
+
+public class PriorityTask extends Task{
+
+}
---
+++ tasks/TimedTask.java
@@ -0,0 +1,9 @@
+package tasks;
+
+public class TimedTask extends Task {
+    private LocalDate deadline;
+    private double durationInHours;
+
+
+
+}
