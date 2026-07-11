bde0dc5351a32203ab1d031431f0d3ebcbf09414
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-07-11 15:54:52 +0100

    Override getStatus() method in TimedTask class

--- tasks/TimedTask.java
+++ tasks/TimedTask.java
@@ -4,6 +4,17 @@ public class TimedTask extends Task {
     private LocalDate deadline;
     private double durationInHours;
 
+    @Override
+    public String getStatus() {
+        if (!isCompleted()){
+            return "Summary:\nTask ID: " + getId() + "\nDescription: " + getDescription() + "\nDeadline: " + deadline +
+                    "\nDuration: " + durationInHours + " hours" + "\nCompletion Status: has not been completed yet";
+        }
+        return "Summary:\nTask ID: " + getId() + "\nDescription: " + getDescription() + "\nDeadline: " + deadline +
+                "\nDuration: " + durationInHours + " hours" + "\nCompletion Status: Completed";
+
+    }
+
 
 
 
