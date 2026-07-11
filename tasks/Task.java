c90559d07a4c11319914861623bd582549188973
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-07-11 15:11:48 +0100

    Create a method to completed unfinished tasks and prevent multiple completions.

--- tasks/Task.java
+++ tasks/Task.java
@@ -1,4 +1,23 @@
 package tasks;
 
 public class Task {
+
+    private final int id;
+    private String description;
+    private boolean completed;
+
+    public void complete() {
+        if (!completed) {
+            completed = true;
+        }
+
+    }
+
+    public String getStatus(int id, ) {
+        return "Task ID: " + id
+    }
+
+
+
+
 }
