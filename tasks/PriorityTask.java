0003a21c6e33580496c606f4bad99f0f8f1a9edd
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-07-11 16:07:03 +0100

    Override the getStatus method in the PriorityTask class

--- tasks/PriorityTask.java
+++ tasks/PriorityTask.java
@@ -4,5 +4,16 @@ public class PriorityTask extends Task{
     private String priority;
     private boolean isApprovalRequired;
 
+    @Override
+    public String getStatus() {
+        if (!isCompleted()){
+            return "Summary:\nTask ID: " + getId() + "\nDescription: " + getDescription() + "\nPriority: " + priority +
+                    "\nRequire Approval? : " + isApprovalRequired + " hours" + "\nCompletion Status: has not been completed yet";
+        }
+        return "Summary:\nTask ID: " + getId() + "\nDescription: " + getDescription() + "\nPriority: " + priority +
+                "\nRequire Approval? : " + isApprovalRequired + " hours" + "\nCompletion Status: Completed";
+
+    }
+
 
 }
