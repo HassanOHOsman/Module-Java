b1b4fa964e25f73e28f4850d032c17ad55251cb0
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-07-11 16:13:55 +0100

    Get rid of irrelevant string portions from the getStatus method.

--- tasks/PriorityTask.java
+++ tasks/PriorityTask.java
@@ -2,16 +2,16 @@ package tasks;
 
 public class PriorityTask extends Task{
     private String priority;
-    private boolean approvalRequired;
+    private boolean isApprovalRequired;
 
     @Override
     public String getStatus() {
         if (!isCompleted()){
             return "Summary:\nTask ID: " + getId() + "\nDescription: " + getDescription() + "\nPriority: " + priority +
-                    "\nRequire Approval? : " + approvalRequired + "\nCompletion Status: has not been completed yet";
+                    "\nRequire Approval? : " + isApprovalRequired + "\nCompletion Status: has not been completed yet";
         }
         return "Summary:\nTask ID: " + getId() + "\nDescription: " + getDescription() + "\nPriority: " + priority +
-                "\nRequire Approval? : " + approvalRequired + "\nCompletion Status: Completed";
+                "\nRequire Approval? : " + isApprovalRequired + "\nCompletion Status: Completed";
 
     }
 
