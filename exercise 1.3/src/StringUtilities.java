c7acaab266942737f54a594896279b1c201d86be
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-07-21 10:44:10 +0100

    Create StringUtilitiesTest a test class with a BeforeEach annotation

---
+++ exercise 1.3/src/StringUtilities.java
@@ -0,0 +1,4 @@
+public class StringUtilities {
+    public boolean isPalindrome(String s) { ... }
+    public List<String> splitCommaSeparatedLine(String s) { ... }
+}
---
+++ exercise 1.3/src/StringUtilitiesTest.java
@@ -0,0 +1,16 @@
+import import org.junit.jupiter.api.BeforeEach;
+
+
+
+public class StringUtilitiesTest {
+
+    StringUtilities stringUtilities;
+
+    @BeforeEach
+    void setUp() {
+        stringUtilities = new StringUtilities();
+    }
+
+
+
+}
