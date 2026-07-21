f129a684a4c9fc78559d8d5b0230c279b8f275dd
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-07-21 11:08:15 +0100

    Create test cases (includeing edge cases) for test method splitCommaSeparatedLine

--- exercise 1.3/src/StringUtilitiesTest.java
+++ exercise 1.3/src/StringUtilitiesTest.java
@@ -1,5 +1,8 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 
@@ -18,6 +21,13 @@ public class StringUtilitiesTest {
         assertTrue(stringUtilities.isPalindrome("mom"), "this word should be palindrome");
         assertTrue(stringUtilities.isPalindrome("B"), "this word should be palindrome"); //edge case
         assertTrue(stringUtilities.isPalindrome(""), "this word should be palindrome");  // edge case
+    }
+
+    @Test
+    void splitCommaSeparatedLine(){
+        assertEquals(List.of("Jan", "Feb", "Mar", "Apr"), stringUtilities.splitCommaSeparatedLine("Jan, Feb, Mar, Apr"), "Comma-separated string should return a list of those strings on its own");
+        assertEquals(List.of("Spring - Summer - Autumn - Winter"), stringUtilities.splitCommaSeparatedLine("Spring - Summer - Autumn - Winter"), "Comma-separated string should return a list of those strings on its own"); //edge case
+        assertEquals(List.of("  ", "  ", "  ", "  "), stringUtilities.splitCommaSeparatedLine("  ,  ,  ,  "), "Comma-separated string should return a list of those strings on its own");// edge case
     }
 
 
