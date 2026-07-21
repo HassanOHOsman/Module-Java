953f8d4118456b13409dfc6e764d8ddeaa71d5a5
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-07-21 17:22:48 +0100

    Answer the question related to this segment of the exercise.

--- exercise1.4/src/main/java/org/example/StringUtilities.java
+++ exercise1.4/src/main/java/org/example/StringUtilities.java
@@ -23,3 +23,9 @@ public class StringUtilities {
 
     }
 }
+
+/*
+Question and Answer:
+Q1. Note that source and test classes should be in separate folders (src/main/java, src/test/java). What do you notice if they are all in src/main/java?
+A1. This creates confusion, specially when it comes to the responsiblity/role of each class.
+*  */
