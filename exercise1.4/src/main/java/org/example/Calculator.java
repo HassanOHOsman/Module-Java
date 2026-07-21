38b7e42f8dcb5ba3781f0b6cf9c586ffd6859e51
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-07-21 17:06:21 +0100

    Add the Calculator and StringUtilities classes as well as their tests classes

---
+++ exercise1.4/src/main/java/org/example/Calculator.java
@@ -0,0 +1,33 @@
+package org.example;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Division by zero");
+        }
+        return a / b;
+    }
+
+    public int square(int a) {
+        return a * a;
+    }
+
+    public double squareRoot(int a) {
+        if (a < 0) {
+            throw new IllegalArgumentException("Square root of negative values is not allowed");
+        }
+        return Math.sqrt(a);
+    }
+}
