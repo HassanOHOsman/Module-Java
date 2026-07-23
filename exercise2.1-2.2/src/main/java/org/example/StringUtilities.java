c36b86b44120930028cf8fda3a8ff8411ad40038
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-07-23 12:32:25 +0100

    Add a javadoc comment associated with the StringUtilities class

--- exercise2.1-2.2/src/main/java/org/example/StringUtilities.java
+++ exercise2.1-2.2/src/main/java/org/example/StringUtilities.java
@@ -1,6 +1,17 @@
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * Represents a string processor with two methods: one that checks if a string is palindrome, while the other splits a
+ * comma-separated string into a list of strings
+ *
+ * @author Hassan Osman
+ * @version 4.0
+ * @since 1.0
+ * @see StringBuilder
+ * @see Arrays
+ * @see List
+ */
 public class StringUtilities {
     public boolean isPalindrome(String s) {
 
