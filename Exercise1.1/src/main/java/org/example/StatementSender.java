e32760a0d4e7bc9543b40afc429442ee262d60f4
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-07-27 14:20:17 +0100

    Add javadoc comments for the both the interface and its abstract method.

--- Exercise1.1/src/main/java/org/example/StatementSender.java
+++ Exercise1.1/src/main/java/org/example/StatementSender.java
@@ -1,7 +1,20 @@
 package org.example;
 
+/**
+ * Defines a contract for sending monthly statements to customers.
+ * Contains an abstract method {@code sendStatement} which will be implemented differently by two extending
+ * interfaces.
+ *
+ * @author  Hassan Osman
+ * @version 1.0
+ * @since 1.0
+ */
 public interface StatementSender {
-
+    /**
+     * Sends monthly statements to customers.
+     *
+     * @param statementContent the content of the statement to be sent
+     */
     String sendStatement(String statementContent);
 }
 
