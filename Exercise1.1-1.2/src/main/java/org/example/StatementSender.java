9bf39e3e0dd324ad14ca53b8cd6b5e38c5ff0f00
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-07-27 23:26:04 +0100

    Answer the first 2 questions from exercise 1.2

--- Exercise1.1-1.2/src/main/java/org/example/StatementSender.java
+++ Exercise1.1-1.2/src/main/java/org/example/StatementSender.java
@@ -21,7 +21,8 @@ public interface StatementSender {
 }
 
 /*
-Questions and Answers:
+Questions and Answers - exercise 1.1:
+
 Q1. What should the return type of this method be?
 A1. There won't be any return, since the method is intended to print to the console. Thus, it should be
     proceeded with "void".
@@ -34,3 +35,15 @@ Q3. Why is this Javadoc important? Think about who we are aiming this javadoc at
 A3. Besides providing description, it let other developers understand the puporse behind the interface
     and its abstract method.
 */
+
+/*
+Questions and Answers - exercise 1.2:
+
+Q1. What is it called when you have two versions of the same method?
+A1. Method Overloading - which is a form of Polymorphism.
+
+Q2. What methods might Statement have? Think about the type of information you’d expect to see on a bank statement.
+A2. getCustomer() method - to get the name of a customer, getBalance() method - to get the balance in the statement,
+    getBank() method - to get the name of the bank and many more.
+
+ */
