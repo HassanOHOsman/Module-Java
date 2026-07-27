98c9c1960ac68db6fd3ff23a1e4bca3d3a542a0a
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-07-27 23:29:27 +0100

    Create two implementations (classes) of Statement interface: AllTransactionsMonthlyStatement and OutgoingTransactionsMonthlyStatement classes

--- Exercise1.1-1.2/src/main/java/org/example/Statement.java
+++ Exercise1.1-1.2/src/main/java/org/example/Statement.java
@@ -2,3 +2,17 @@ package org.example;
 
 public interface Statement {
 }
+
+
+
+/*
+Questions and Answers - exercise 1.2:
+
+Q1. What is it called when you have two versions of the same method?
+A1. Method Overloading - which is a form of Polymorphism.
+
+Q2. What methods might Statement have? Think about the type of information you’d expect to see on a bank statement.
+A2. getCustomer() method - to get the name of a customer, getBalance() method - to get the balance in the statement,
+    getBank() method - to get the name of the bank and many more.
+
+ */
--- Exercise1.1-1.2/src/main/java/org/example/StatementSender.java
+++ Exercise1.1-1.2/src/main/java/org/example/StatementSender.java
@@ -36,14 +36,4 @@ A3. Besides providing description, it let other developers understand the pupors
     and its abstract method.
 */
 
-/*
-Questions and Answers - exercise 1.2:
 
-Q1. What is it called when you have two versions of the same method?
-A1. Method Overloading - which is a form of Polymorphism.
-
-Q2. What methods might Statement have? Think about the type of information you’d expect to see on a bank statement.
-A2. getCustomer() method - to get the name of a customer, getBalance() method - to get the balance in the statement,
-    getBank() method - to get the name of the bank and many more.
-
- */
