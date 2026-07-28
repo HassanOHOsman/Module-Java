fa4e395349d8e731605616fcb86ed407643efa27
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-07-28 12:50:57 +0100

    Override the 2nd version of the sendStatement with Statement arg into both classes: EmailStatementSender and LetterStatementSender.

--- Exercise1.1-1.2/src/main/java/org/example/EmailStatementSender.java
+++ Exercise1.1-1.2/src/main/java/org/example/EmailStatementSender.java
@@ -6,4 +6,9 @@ public class EmailStatementSender implements StatementSender{
     public void sendStatement(String statementContent) {
         System.out.println(statementContent + " statement has been successfully emailed to you.");
     }
+
+    @Override
+    public void sendStatement(Statement statement) {
+        System.out.println("The statement has been successfully emailed to you.");
+    }
 }
--- Exercise1.1-1.2/src/main/java/org/example/LetterStatementSender.java
+++ Exercise1.1-1.2/src/main/java/org/example/LetterStatementSender.java
@@ -6,4 +6,9 @@ public class LetterStatementSender implements StatementSender {
     public void sendStatement(String statementContent) {
         System.out.println(statementContent + " statement has been successfully posted to your address.");
     }
+
+    @Override
+    public void sendStatement(Statement statement) {
+        System.out.println("The statement has been successfully posted to your address.");
+    }
 }
