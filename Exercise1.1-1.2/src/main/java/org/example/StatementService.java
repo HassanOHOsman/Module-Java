f1c825c6c24e230ca507713b4bc5a02a73999474
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-07-29 12:34:50 +0100

    Update the methods that create and send both types of statements inside the StatementService class.

--- Exercise1.1-1.2/src/main/java/org/example/StatementService.java
+++ Exercise1.1-1.2/src/main/java/org/example/StatementService.java
@@ -1,21 +1,20 @@
 package org.example;
 
+import java.time.LocalDate;
+
 public class StatementService {
 
 
 
-    public void generateAndSendOutgoingStatement(){
-
-        Statement statement = new OutgoingTransactionsMonthlyStatement();
-        StatementSender statementSender = new StatementSender();
+    public void generateAndSendOutgoingStatement(StatementSender statementSender){
+        Statement statement = new OutgoingTransactionsMonthlyStatement("Hassan Osman", "Monzo", 650.95, LocalDate.now());
         StatementDeliveryService statementDeliveryService = new StatementDeliveryService();
         statementDeliveryService.deliverStatement(statement, statementSender);
-
     }
 
-    public void generateAndSendAllTransactionsStatement(){
 
-        Statement statement = new AllTransactionsMonthlyStatement();
+    public void generateAndSendAllTransactionsStatement(StatementSender statementSender){
+        Statement statement = new AllTransactionsMonthlyStatement("Mishaal AL-Saud", "Coutts", 97309.05, LocalDate.of(2026, 03, 31));
         StatementDeliveryService statementDeliveryService = new StatementDeliveryService();
         statementDeliveryService.deliverStatement(statement, statementSender);
     }
