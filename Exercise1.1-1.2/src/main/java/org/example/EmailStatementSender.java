d35b7987d8537a3c68e800fa092d06778acaa4ab
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-07-29 11:56:49 +0100

    Update the 2nd sendStatement verison in EmailStatementSender so that it uses the methods (getters) that statement expose.

--- Exercise1.1-1.2/src/main/java/org/example/EmailStatementSender.java
+++ Exercise1.1-1.2/src/main/java/org/example/EmailStatementSender.java
@@ -9,6 +9,10 @@ public class EmailStatementSender implements StatementSender{
 
     @Override
     public void sendStatement(Statement statement) {
-        System.out.println("The statement has been successfully emailed to you.");
+        System.out.println("Statement Date: " + statement.getStatementDate());
+        System.out.println("Customer Name: " + statement.getCustomerName());
+        System.out.println("Bank Name: " + statement.getBankName());
+        System.out.println("Balance: £" + statement.getBalance());
+        System.out.println("Your monthly statement has been emailed to you successfully.");
     }
 }
