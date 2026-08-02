18e7ebfa8749512d8395165847223f80d7050396
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-08-02 17:37:11 +0100

    Update data sets (arrays)

---
+++ Exercise2.1-2.2/src/main/java/org/example/Main.java
@@ -0,0 +1,25 @@
+package org.example;
+
+
+import java.util.*;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        String[] firstNames = {"Anuket", "Eryn", "Filip", "Raquel", "Eryn", "Ali", "Sam"};
+        String[] lastNames = {"Hassan", "Doe", "Pappas", "Clarke", "Jones", "Hassan", "Clarke"};
+        int[] grades = {22, 45, 35, 22, 41, 48, 39};
+
+        StudentGrades studentGrades = new StudentGrades(firstNames, lastNames, grades);
+
+        System.out.println(Arrays.toString(studentGrades.getFirstNames()));
+        System.out.println(studentGrades.getDistinctFirstNames(firstNames));
+        System.out.println(studentGrades.getDistinctFirstNamesInAlphabeticalOrder(firstNames));
+        System.out.println(studentGrades.getLastNameToGrade(lastNames, grades));
+
+    }
+
+}
+
+
