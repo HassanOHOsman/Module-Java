09a34282be583438ca5526f879bda8a6929a4d91
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-08-03 10:00:07 +0100

    Answer all reflection questions to exercise 3.2

--- Exercises/src/main/java/org/example/StudentGrades.java
+++ Exercises/src/main/java/org/example/StudentGrades.java
@@ -62,12 +62,49 @@ public class StudentGrades {
 }
 
 /*
-Reflection
+Reflection - Exercise 2.1
 
-Suppose there are a set of twins in the class, so there are some duplicate lastNames. Can we still build a map of
-students to their grades?
+Question: Suppose there are a set of twins in the class, so there are some duplicate lastNames. Can we still build a
+          map of students to their grades?
 
 Answer: perhaps we might need a 4th array that provide unique values that could be associated with each student.
-Something like an array of student's ID numbers can work. Otherwise, maybe there's a way for the key to be customized
-to include both the first and last names of each student in such cases.
+        Something like an array of student's ID numbers can work. Otherwise, maybe there's a way for the key to be
+        customized to include both the first and last names of each student in such cases.
+
+
+
+Reflections - Exercise 2.3
+Think about the following questions, make notes and be prepared to talk through your thoughts in the workshop.
+
+Q1. Would it have been possible to use Arrays to model the same data?
+A1. Yes, it would.
+
+Q2. Would it have been easier, harder or the same? Why?
+A2. It would have been harder. This is because arrays are not dynamic, they are of fixed size, therefore, adding and
+    removing elements would have been more challenging.
+
+Q3. What was your thought process when deciding which data structure was most appropriate for modelling particular data?
+What things did you consider?
+A3. If the goal was to have data represented in key-value pair, then I'd use HashMap (other implementations of Map
+    interface can work. If the goal was to have data as distinct values, then I'd use HashSet (other Set interface
+    implementations can work), and if order mattered, then I'd go for TreeSet instead. If the data insertion order and
+     storing order matters, then, I'd go for an ArrayList (other List implementation can work).
+
+Q4. Suppose the initial data set was input incorrectly and the same student’s name and grade was input twice. How would
+    your Map<Student, Grade> handle this? Why?
+A4. Since Student is key here, and Student class has implemented both equals() and hashCode() methods, then they will
+    consider duplicate student objects as the same object. Therefore, Map<Student, Grade> treats them as the same key.
+
+Q5. What is the difference between variables: a) List firstNames, b) List<String> firstNames,
+c) ArrayList<String> firstNames ? Which is preferable? Bonus: how about List<?> firstNames ?
+A5. a) is a list interface that may contain different data types
+    b) is a list interface of Strings ONLY
+    c) is an array list - implementing the List interface - and cntains Strings only
+
+    I prefer List<String> firstNames because interfaces are more flexible, allowing loose coupling and multiple
+    inheritance. Also having the input type - String - explicitly stated ensure that no other data type can be passed.
+
+   Regarding List<?> firstNames perhaps it's a list of certain type which is yet to be confirmed.
+
+
  */
