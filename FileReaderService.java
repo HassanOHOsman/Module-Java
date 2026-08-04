e46524b0c45ea40d30b4773aedeeb6ca1b3987f5
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-08-04 16:25:19 +0100

    Create a FileReaderService class with a method readFile(String filename) that attempts to read a file using BufferedReader

---
+++ FileReaderService.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class FileReaderService {
+    String readFile(String filename) {
+
+        BufferedReader bufferedReader = new BufferedReader(filename);
+
+
+
+
+
+
+    }
+}
