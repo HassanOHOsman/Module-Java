e78e301aabe154935c03c51ac5c76c4debc0367c
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-08-04 16:49:21 +0100

    Handle the IOException exception

--- FileReaderService.java
+++ FileReaderService.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class FileReaderService {
     String readFile(String filename) {
@@ -9,6 +10,8 @@ public class FileReaderService {
             BufferedReader bufferedReader = new BufferedReader(new FileReader(filename));
         } catch(FileNotFoundException e) {
             System.out.println("File not found");
+        } catch (IOException e) {
+            System.out.println("Could not read file");
         }
 
 
