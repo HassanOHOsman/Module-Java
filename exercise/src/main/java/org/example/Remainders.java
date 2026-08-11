a545bc3bdb6bfa2ab268422096579a9946fd87b6
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-08-11 22:51:37 +0100

    Complete implementing printRemainders method to successfully log out the remainders of the division of numbers from 1 -20 inclusive by any divisor (integer), comma-separated.

--- exercise/src/main/java/org/example/Remainders.java
+++ exercise/src/main/java/org/example/Remainders.java
@@ -2,7 +2,21 @@ package org.example;
 
 public class Remainders {
 
-    public void printRemainders(int) {
+    public void printRemainders(int divisor) {
+        String remainders = "";
+        for (int i = 1; i <= 20; i ++) {
+            int remainder = i % divisor;
+            if (i < 20) {
+                remainders += remainder + ",";
+            } else {
+                remainders += remainder
+            }
+
+        }
+        remainders += "\n";
+
+        System.out.println(remainders);
+
 
     }
 }
