99e97296d91e4d1711fd8a8fb5e920ce7fbb114a
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-08-13 11:52:25 +0100

    Create Rental class with several core fields in it.

---
+++ exercise/src/main/java/org/example/Rental.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import java.time.LocalDate;
+
+public class Rental {
+    private LocalDate dueDate;
+    private double capitalAmount;
+    private double interestAmount;
+    private boolean paid;
+
+
+
+}
