ab37a912e0102f2520f0f4ff35236318731fd70b
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-08-13 11:18:12 +0100

    Create Contract class with a few essential fields.

---
+++ exercise/src/main/java/org/example/Contract.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.time.LocalDate;
+
+public class Contract {
+    private String customerName;
+    private String customerAge;
+    private LocalDate startDate;
+    private double carPrice;
+    private int contractLengthYears;
+
+}
