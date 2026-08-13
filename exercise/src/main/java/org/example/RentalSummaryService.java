d0eb46c3b5cf23ede6e6b0a7de32ed69c576e129
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-08-13 15:56:40 +0100

    Start implementing generateRentalSummary method

--- exercise/src/main/java/org/example/RentalSummaryService.java
+++ exercise/src/main/java/org/example/RentalSummaryService.java
@@ -1,13 +1,30 @@
 package org.example;
 
+import java.util.ArrayList;
 import java.util.Optional;
+import java.util.List;
+
+import static org.example.Contract.ContractLengthYears.One_YEAR;
 
 public class RentalSummaryService {
 
     private Optional<RentalSummary> generateRentalSummary(Contract contract) {
-        RentalSummary rentalSummary = new RentalSummary();
+
+        RentalCalculator rentalCalculator;
+
+        if (contract.getContractLengthYears() == One_YEAR) {
+            rentalCalculator = new OneYearContractRentalGenerator();
+        } else {
+            rentalCalculator = new ThreeYearContractRentalGenerator();
+        }
+
+        List<Rental> rentals = rentalCalculator.generateRentals(contract);
+
+        RentalSummary rentalSummary = new RentalSummary(contract.getCustomerName(), contract.getCustomerAge(), contract.getStartDate(), );
 
     }
+
+
 
     void printRentalSummary(Contract contract) {
 
