a03bb50a1f1160ee18cfefc58dc5a87490184fbc
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-08-13 13:10:49 +0100

    inside generateRentals method, work out the monthly capital

--- exercise/src/main/java/org/example/OneYearContractRentalGenerator.java
+++ exercise/src/main/java/org/example/OneYearContractRentalGenerator.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,10 +9,7 @@ public class OneYearContractRentalGenerator implements RentalCalculator {
     List<Rental> generateRentals(Contract contract) {
         List<Rental> rentals = new ArrayList<>();
 
-        Contract contract1 = new Contract();
-        contract1
-
-
+        double monthlyCapital = contract.getCarPrice() / 12;
 
         return rentals;
 
