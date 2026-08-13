73723c02a3cc2fae3cbf5e44b910e7fa7592d74e
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-08-13 14:44:41 +0100

    create RentalSummaryService class and add 3 method signatures into it (to be implemented).

---
+++ exercise/src/main/java/org/example/RentalSummaryService.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import java.util.Optional;
+
+public class RentalSummaryService {
+
+    private Optional<RentalSummary>
+    generateRentalSummary(Contract contract)
+    void printRentalSummary(Contract contract)
+}
