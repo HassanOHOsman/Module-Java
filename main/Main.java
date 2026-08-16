e462a81b156fbf2e54d890c88f528c49bacd4eb0
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-08-16 23:13:30 +0100

    Update and remove args from some of the object instantiations since the source code has updated.

--- main/Main.java
+++ main/Main.java
@@ -10,16 +10,16 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        Task task = new Task(333,"Email customised onboarding guide to new-hires", true);
+        Task task = new Task("Email customised onboarding guide to new-hires", true);
         System.out.println(task.getStatus());
 
-        PriorityTask priorityTask = new PriorityTask(404, "Finance Team experiencing tech issues", false, "High", false);
+        PriorityTask priorityTask = new PriorityTask("Finance Team experiencing tech issues", false, "High", false);
         System.out.println(priorityTask.getStatus());
 
         TimedTask timedTask = new TimedTask(159, "Compliance Team needs new screens", false, LocalDate.of(2026, 8, 1), 72);
         System.out.println(timedTask.getStatus());
 
-        TaskManager taskManager = new TaskManager(new Task[10], 0);
+        TaskManager taskManager = new TaskManager();
         taskManager.addTask(task);
         taskManager.addTask(priorityTask);
         taskManager.addTask(timedTask);
