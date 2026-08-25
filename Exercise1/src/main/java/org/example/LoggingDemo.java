eb77c756a9aec30c9fdde3e26f56d1e13f73f857
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-08-25 23:17:02 +0100

    Add a loop to the process and log from within the loop every 10th iteration.

--- Exercise1/src/main/java/org/example/LoggingDemo.java
+++ Exercise1/src/main/java/org/example/LoggingDemo.java
@@ -11,5 +11,11 @@ public class LoggingDemo {
         log.debug("This is a debug message that you should NOT see yet.");
         log.warn("Low disk space!");
         log.error("Something went wrong.", new RuntimeException("Test error"));
+
+        for (int i = 1; i <= 100; i ++) {
+            if (i % 10 == 0)  {
+                log.info("This is the {}th iteration", i);
+            }
+        }
     }
 }
