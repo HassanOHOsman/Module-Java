76c0f10180e5037607e7716616319c5e9dd81a1f
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-08-25 16:13:06 +0100

    Copy LoggingDemo class and pom.xml files as per exercise 1

---
+++ Exercise1/src/main/java/org/example/LoggingDemo.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LoggingDemo {
+    private static final Logger log = LoggerFactory.getLogger(LoggingDemo.class);
+
+    public static void main(String[] args) {
+        log.info("Application starting...");
+        log.debug("This is a debug message that you should NOT see yet.");
+        log.warn("Low disk space!");
+        log.error("Something went wrong.", new RuntimeException("Test error"));
+    }
+}
