cc32ea1b9b8bf659a681cc503a4a455adf832150
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-08-26 09:39:18 +0100

    1. Copy the configuration file and  RequestProcessor class file as per exercise 2.
    2. Set up pom.xml with slf4j & logback as dependencies.

---
+++ Exercise2/src/main/java/org/example/RequestProcessor.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+
+import java.util.UUID;
+
+public class RequestProcessor {
+    private static final Logger log = LoggerFactory.getLogger(RequestProcessor.class);
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 3; i++) {
+            simulateRequest(i);
+        }
+    }
+
+    private static void simulateRequest(int n) {
+        String requestId = UUID.randomUUID().toString();
+        MDC.put("requestId", requestId);
+
+        log.info("Processing request {}", n);
+
+        try {
+            if (n == 2) throw new IllegalArgumentException("Bad input!");
+        } catch (Exception e) {
+            log.error("Error while processing", e);
+        } finally {
+            MDC.clear();
+        }
+    }
+}
